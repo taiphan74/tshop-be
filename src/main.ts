d066c5a01c1d6aba932a4d43d6369ac1ad8809ae
TaiPhanCoder <lonelyguitarist2745@gmail.com>
2025-09-19 09:02:21 +0700

    feat: add Swagger documentation and dotenv support; remove unused files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('TShop API')
+      .setDescription('TShop backend API documentation')
+      .setVersion('1.0')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          in: 'header',
+        },
+        'access-token',
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    // apply global security so Swagger UI shows the auth lock for all endpoints
+    (document as any).security = [{ 'access-token': [] }];
+
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    });
+  }
+
   await app.listen(process.env.PORT ?? 3000);
+  const url = await app.getUrl();
+  const port = process.env.PORT ?? 31111;
+  console.log(`Application running at: http://localhost:${port}`);
 }
+
 bootstrap();
